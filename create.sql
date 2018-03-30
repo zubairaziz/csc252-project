048f13c31a5661b08b0de86144da3517f6835468
Ziqing Liang <ziqingliang@vpn13-9.net.rochester.edu>
2018-03-30 14:51:43 -0400

    updated create.sql

--- create.sql
+++ create.sql
@@ -7,3 +7,49 @@ CREATE TABLE User(
     status INT(1) NOT NULL , 
     PRIMARY KEY (userID)
     );
+
+CREATE ProfRoom(
+    professorID INT(11) NOT NULL,
+    roomNumber VARCHAR(10) NOT NULL,
+    PRIMARY KEY(professorID),
+    FOREIGN KEY(professorID) REFERENCES User(userID) ON DELETE CASCADE
+    );
+
+CREATE Courses(
+    courseID VARCHAR(10) NOT NULL,
+    courseName VARCHAR(50) NOT NULL,
+    PRIMARY KEY(courseID)
+    );
+
+CREATE Teach(
+    professorID INT(11) NOT NULL,
+    courseID VARCHAR(10) NOT NULL,
+    PRIMARY KEY(professorID,courseID),
+    FOREIGN KEY(courseID) REFERENCES Courses(courseID) ON DELETE CASCADE,
+    FOREIGN KEY(professorID) REFERENCES Users(userID) ON DELETE CASCADE
+    );
+
+CREATE Availability(
+    professorID INT(11) NOT NULL,
+    day INT(1) NOT NULL,
+    starts TIME,
+    ends TIME,
+    PRIMARY KEY(professorID, day),
+    FOREIGN KEY(professorID) REFERENCES Users(userID) ON DELETE CASCADE 
+    );
+
+CREATE Appointment(
+    
+    professorID INT(11) NOT NULL,
+    studentID INT(11) NOT NULL,
+    date DATE NOT NULL,
+    starts TIME NOT NULL,
+    ends TIME NOT NULL,
+    apptID INT(11) NOT NULL AUTO_INCREMENT,
+    purpose TEXT NOT NULL,
+    status INT(1) NOT NULL,
+    PRIMARY KEY(professorID, studentID, date , starts, ends),
+    FOREIGN KEY(professorID) REFERENCES Users(userID) ON DELETE CASCADE,
+    FOREIGN KEY(studentID) REFERENCES Users(userID) ON DELETE CASCADE
+);
+
