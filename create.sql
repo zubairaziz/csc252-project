0bae25bee5e87d586cad7269d3952c27c69cf807
Ziqing Liang <ziqingliang@vpn13-9.net.rochester.edu>
2018-03-30 14:28:49 -0400

    Updated create.sql

--- create.sql
+++ create.sql
@@ -1,3 +1,9 @@
-CREATE TABLE User ()
-
-asdasdasda
+CREATE TABLE User(
+    userID INT(11) NOT NULL AUTO_INCREMENT , 
+    email VARCHAR(50) NOT NULL , 
+    password VARCHAR(50) NOT NULL , 
+    firstName VARCHAR(25) NOT NULL , 
+    lastName VARCHAR(25) NOT NULL , 
+    status INT(1) NOT NULL , 
+    PRIMARY KEY (userID)
+    );
